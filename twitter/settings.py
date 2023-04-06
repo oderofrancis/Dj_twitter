da9f53d9e80bbb23d1f11c225e2b14e4c364f47d
oderofrancis <francisodero10@gmail.com>
2023-04-06 10:44:57 +0300

    Bootstrapping the Django

--- twitter/settings.py
+++ twitter/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# crispy form
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
